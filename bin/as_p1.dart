e432a48a80d51a6694e9bc382b89c7880b01a3c4
jibinbala <jibinep36@gmail.com>
2023-09-18 12:41:08 +0530

    Initial commit

---
+++ bin/as_p1.dart
@@ -0,0 +1,7 @@
+void main()
+{
+  print("my self jibin");
+  print("i am 23 years old");
+  print("objectmy mobile no is 9188800898");
+  print(6.5);
+}
---
+++ bin/datatype.dart
@@ -0,0 +1,14 @@
+void main()
+{
+  String name="jibin";
+  int age=23;
+  double cgpa=6.5;
+  String email="jibinep36@gmail.com";
+
+
+
+  print("my age is $age");
+  print("my name is $name");
+  print("my cgpa is $cgpa");
+  print("my mail is $email");
+}
